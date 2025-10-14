13844432c3cfd70cd207c485cb7021fea69bee0c
SOMESH <someshbiswal2308@gmail.com>
2025-10-14 22:51:05 +0530

    First Add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My CI/CD Frontend</h1>
+      <p>This is a simple one-page React + Vite app deployed via Jenkins.</p>
+      <button onClick={() => alert("Build Successful!")}>Click Me</button>
+    </div>
+  );
+}

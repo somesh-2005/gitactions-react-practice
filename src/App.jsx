76683ebcea571dc5793d37569c603d2c2d68e2a9
SOMESH <someshbiswal2308@gmail.com>
2025-10-18 08:23:16 +0530

    Project v2.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="container">
       <h1>Welcome to My CI/CD Frontend</h1>
+      <h1>Hii I am Somesh Swadhin Biswal</h1>
       <p>This is a simple one-page React + Vite app deployed via Jenkins.</p>
       <button onClick={() => alert("Build Successful!")}>Click Me</button>
     </div>
